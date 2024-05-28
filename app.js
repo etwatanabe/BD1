4fa199dbb912885678a279d3341a052cbfa80dd3
Takeshi Watanabe <wattks@hotmail.com>
2024-05-28 14:02:54 -0300

    mudança de tema

--- app.js
+++ app.js
@@ -1,52 +1,68 @@
-// app.js
 const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
+const path = require('path');
+
 const app = express();
-const port = 3000;
+const db = new sqlite3.Database('./database/musicas.db');
 
+// Configurar EJS como template engine
 app.set('view engine', 'ejs');
-app.use(express.static('public'));
+app.set('views', path.join(__dirname, 'views'));
 
-const db = new sqlite3.Database('./database/hamburgueria.db');
+// Middleware para servir arquivos estáticos
+app.use(express.static(path.join(__dirname, 'public')));
 
+// Rota principal
 app.get('/', (req, res) => {
-    const ingredienteId = req.query.ingredientes;
-    console.log(ingredienteId);
-    let query = `SELECT hamburgueres.nome, hamburgueres.descricao, hamburgueres.preco
-                 FROM hamburgueres
-                 JOIN hamburgueres_ingredientes ON hamburgueres.id = hamburgueres_ingredientes.hamburguer_id
-                 JOIN ingredientes ON hamburgueres_ingredientes.ingrediente_id = ingredientes.id
-                 WHERE 1=1
-                 `;
+    const artistaSelecionado = req.query.artista;
+    const generoSelecionado = req.query.genero;
+    let query = `SELECT musicas.titulo, musicas.duracao, generos.nome AS genero, artistas.nome AS artista
+                 FROM musicas 
+                 JOIN artistas ON musicas.artista_id = artistas.id
+                 JOIN generos ON artistas.genero_id = generos.id`;
+    // console.log(artistaSelecionado, generoSelecionado);
 
-    if (ingredienteId) {
-        for(let ingrediente in ingredienteId) {
-            query += ` AND ingredientes.id = ${ingredienteId[ingrediente]}`;
+    if (artistaSelecionado) {
+        query += ` WHERE artistas.id = ${artistaSelecionado}`;
+    }
+
+    if (generoSelecionado) {
+        if(artistaSelecionado) {
+            query += ` AND generos.id = ${generoSelecionado}`;
+        }
+        else {
+            query += ` WHERE generos.id = ${generoSelecionado}`;
         }
     }
 
-    query += ` GROUP BY hamburgueres.id`;
-
-    db.all(query, [], (err, hamburgueres) => {
+    db.all(query, [], (err, musicas) => {
         if (err) {
-            return console.error(err.message);
+            console.error(err.message);
+            res.status(500).send("Erro no servidor");
+            return;
         }
 
-        db.all(`SELECT * FROM ingredientes`, [], (err, ingredientes) => {
+        db.all(`SELECT * FROM artistas`, [], (err, artistas) => {
             if (err) {
-                return console.error(err.message);
+                console.error(err.message);
+                res.status(500).send("Erro no servidor");
+                return;
             }
 
-            res.render('index', {
-                ingredienteId,
-                hamburgueres,
-                ingredientes
+            db.all(`SELECT * FROM generos`, [], (err, generos) => {
+                if (err) {
+                    console.error(err.message);
+                    res.status(500).send("Erro no servidor");
+                    return;
+                }
+
+            res.render('index', { musicas, artistas, generos, artistaSelecionado, generoSelecionado });
             });
         });
     });
-    console.log(query);
 });
 
-app.listen(port, () => {
-    console.log(`App running at http://localhost:${port}`);
+// Iniciar o servidor
+app.listen(3000, () => {
+    console.log(`Servidor rodando em http://localhost:3000`);
 });
---
+++ db.js
@@ -0,0 +1,63 @@
+// init-db.js
+const sqlite3 = require('sqlite3').verbose();
+
+const db = new sqlite3.Database('./database/musicas.db');
+
+db.serialize(() => {
+    db.run(`CREATE TABLE IF NOT EXISTS generos (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        nome TEXT NOT NULL
+    );`)
+
+    db.run(`CREATE TABLE IF NOT EXISTS artistas (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        nome TEXT NOT NULL,
+        genero_id INTEGER,
+        FOREIGN KEY (genero_id) REFERENCES generos(id)
+    );`);
+    
+    db.run(`CREATE TABLE IF NOT EXISTS musicas (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        titulo TEXT NOT NULL,
+        duracao TIME NOT NULL,
+        artista_id INTEGER,
+        FOREIGN KEY (artista_id) REFERENCES artistas(id)
+    );`);
+
+    db.run(`INSERT INTO generos (nome) VALUES 
+        ('Pop'),
+        ('Reggae'),
+        ('Hip Hop'),
+        ('Rock'),
+        ('R&B');
+    `);
+
+    db.run(`INSERT INTO artistas (nome, genero_id) VALUES 
+        ('Michael Jackson', 1), -- Pop
+        ('Madonna', 1), -- Pop
+        ('Bob Marley', 2), -- Reggae
+        ('Eminem', 3), -- Hip Hop
+        ('Adele', 1), -- Pop
+        ('Elvis Presley', 4), -- Rock
+        ('Beyoncé', 5); -- R&B
+    `);
+
+    db.run(`INSERT INTO musicas (titulo, duracao, artista_id) VALUES 
+        ('Thriller', '00:05:57', 1), -- Michael Jackson
+        ('Like a Virgin', '00:03:38', 2), -- Madonna
+        ('Redemption Song', '00:03:48', 3), -- Bob Marley
+        ('Lose Yourself', '00:05:20', 4), -- Eminem
+        ('Rolling in the Deep', '00:03:48', 5), -- Adele
+        ('Can''t Help Falling in Love', '00:03:01', 6), -- Elvis Presley
+        ('Halo', '00:04:21', 7); -- Beyoncé
+        ('Billie Jean', '00:04:54', 1), -- Michael Jackson
+        ('Vogue', '00:05:19', 2), -- Madonna
+        ('No Woman, No Cry', '00:03:00', 3), -- Bob Marley
+        ('Stan', '00:06:44', 4), -- Eminem
+        ('Someone Like You', '00:04:45', 5), -- Adele
+        ('Jailhouse Rock', '00:02:29', 6), -- Elvis Presley
+        ('Single Ladies (Put a Ring on It)', '00:03:13', 7); -- Beyoncé
+    `);
+});
+
+db.close();
